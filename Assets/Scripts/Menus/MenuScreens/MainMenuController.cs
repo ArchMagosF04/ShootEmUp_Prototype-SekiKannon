4aea6beea144c907fa03c2177f6fa0e351fea97a
ArchMagosF04 <francochialvo2004@gmail.com>
2024-11-11 10:08:34 -0300

    final touches

--- Assets/Scripts/Menus/MenuScreens/MainMenuController.cs
+++ Assets/Scripts/Menus/MenuScreens/MainMenuController.cs
@@ -5,29 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
-    [Header("Levels To Load")]
-    public string newGameLevel;
-    private string levelToLoad;
-
-    [SerializeField] private GameObject noSavedGameDialog = null;
+    [SerializeField] public string newGameLevel;
 
     public void StartGame()
     {
         LoadingManager.Instance.LoadScene(newGameLevel);
-    }
-
-    public void LoadGame()
-    {
-        if(PlayerPrefs.HasKey("SavedLevel"))
-        {
-            levelToLoad = PlayerPrefs.GetString("SavedLevel");
-
-            SceneManager.LoadScene(levelToLoad);
-        }
-        else
-        {
-            noSavedGameDialog.SetActive(true);
-        }
     }
 
     public void ExitGame()
